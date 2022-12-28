5e5e7dbaee22afbeb671cbe1c1ab5bd4ce768480
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-12-27 21:04:17 -0300

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Components/Layout/Footer/Footer";
+import Header from "./Components/Layout/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
