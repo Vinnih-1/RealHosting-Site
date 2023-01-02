c99f5282eda9bc1aedb514d1c6cb41aacd8fea05
Mikael <mikaelcandido71@gmail.com>
2023-01-02 17:16:41 -0300

    Chamando componentes que foram criados
    Chamando os dois componentes que foram criados para a página principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Footer from "./Components/Layout/Footer/Footer";
 import Header from "./Components/Layout/Header/Header";
+import Banner from "./Components/Layout/Banner/Banner";
+import About from "./Components/Layout/About/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <About />
       <Footer />
     </div>
   );
