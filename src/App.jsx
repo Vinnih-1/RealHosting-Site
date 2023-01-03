b39497b2c829cf2398d6be21f3115e765257edff
Mikael <mikaelcandido71@gmail.com>
2023-01-03 16:49:21 -0300

    Chamando o componente Plans
    Chamando o componente Plans

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./Components/Layout/Footer/Footer";
 import Header from "./Components/Layout/Header/Header";
 import Banner from "./Components/Layout/Banner/Banner";
 import About from "./Components/Layout/About/About";
+import Plans from "./Components/Layout/Plans/Plans";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Banner />
       <About />
+      <Plans />
       <Footer />
     </div>
   );
