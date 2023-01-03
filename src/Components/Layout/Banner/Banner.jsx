ad6ea9fdd55b54e8915138723774e27aa0ff6999
Mikael <mikaelcandido71@gmail.com>
2023-01-03 16:46:09 -0300

    Adicionando um ClassName
    Adicionando um ClassName na tag h1

--- src/Components/Layout/Banner/Banner.jsx
+++ src/Components/Layout/Banner/Banner.jsx
@@ -3,7 +3,7 @@ export const Banner = () => {
     return (
         <section className='section_banner'>
             <div className='container_banner'>
-                <h1>Bem vindo a melhor plataforma de
+                <h1 className='title_banner'>Bem vindo a melhor plataforma de
                     Hospedagem do mercado.</h1>
                 <p className='text_banner'>Hospede o seu servidor hoje mesmo em um ambiente
                     com servidores 100% em Armazenamento SSD e
