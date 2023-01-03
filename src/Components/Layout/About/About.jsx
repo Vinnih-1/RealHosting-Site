cf8b4e4ad5adadced65a5f1c87492e155644add6
Mikael <mikaelcandido71@gmail.com>
2023-01-03 16:51:01 -0300

    Editando o componente About, criando um slide
    Criando um slide com o framer-motion do react

--- src/Components/Layout/About/About.jsx
+++ src/Components/Layout/About/About.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState, useRef} from 'react'
+import {motion} from 'framer-motion'
 import suport from './../../../Img/about/suporte.png'
 import protection from './../../../Img/about/protecao.png'
 import host from './../../../Img/about/hospedagem.png'
@@ -5,29 +7,44 @@ import quality from './../../../Img/about/qualidade.png'
 import system from './../../../Img/about/sistemas.png'
 import './About.css'
 export const About = () => {
+
+    const carousel = useRef();
+    const [width, setWidth] = useState();
+
+    useEffect(()=>{  
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+}, [])
+    
     return (
-        <section className='section_about'>
-            <div className='container_about'>
+        <motion.section className='section_about' ref={carousel} whileTap={{cursor: "grabbing"}} >
+           <motion.div className='container_cards' 
+           drag="x" 
+           dragConstraints={{right:0, left:-width}}
+           initial={{x: 100}}
+           animate={{x: 0}}
+           >
+           <motion.div className='container_about'>
                 <img src={suport} alt="Suporte especializado" />
                 <p className='text_about'>Suporte <br></br> Especializado</p>
-            </div>
-            <div className='container_about'>
+            </motion.div>
+            <motion.div className='container_about'>
                 <img src={protection} alt="Proteção de Dados" />
                 <p className='text_about'>Proteção de<br></br> Dados</p>
-            </div>
-            <div className='container_about'>
+            </motion.div>
+            <motion.div className='container_about'>
                 <img src={host} alt="Hospedagem Licenciada" />
                 <p className='text_about'>Hospedagem<br></br> Licenciada</p>
-            </div>
-            <div className='container_about'>
+            </motion.div>
+            <motion.div className='container_about'>
                 <img src={quality} alt="Qualidade e Perfomance" />
                 <p className='text_about'>Qualidade &<br></br> Perfomance</p>
-            </div>
-            <div className='container_about'>
+            </motion.div>
+            <motion.div className='container_about'>
                 <img src={system} alt="Sistemas Inteligentes" />
                 <p className='text_about'>Sistemas<br></br> Inteligentes</p>
-            </div>
-        </section>
+            </motion.div>
+           </motion.div>
+        </motion.section>
     )
 }
 export default About
