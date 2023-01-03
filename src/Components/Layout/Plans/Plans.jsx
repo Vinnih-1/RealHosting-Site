d3bbc5f1d5f0fcd50a62db7691143b8441d45efe
Mikael <mikaelcandido71@gmail.com>
2023-01-03 16:42:50 -0300

    Criando Componente Plans
    Iniciando Componente Plans que irá conter os principais planos da hospedagem

---
+++ src/Components/Layout/Plans/Plans.jsx
@@ -0,0 +1,22 @@
+import './Plans.css'
+export const Plans = () => {
+    return (
+        <section className="section_plans">
+            <div>
+                <div>
+                    <h1>Confira os nossos principais plano totalmente ideal para o seu projeto</h1>
+                </div>
+                <div>
+                    <p>Promova hoje seu servidor com uma
+                        hospedagem com umaalta qualidade e proteção
+                        contra qualquer tipo de ataque.
+                    </p>
+                </div>
+            </div>
+            <div className='card_plans'>
+
+            </div>
+        </section>
+    )
+}
+export default Plans
