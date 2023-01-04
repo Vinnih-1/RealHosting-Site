059645ab4c029e3e087c3081fae0133843a35966
Mikael <mikaelcandido71@gmail.com>
2023-01-04 16:44:47 -0300

    Criando a página Home
    Criando a página Home e chamando os componentes da home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import Banner from "./../Components/Layout/Banner/Banner";
+import About from "./../Components/Layout/About/About";
+import Plans from "./../Components/Layout/Plans/Plans";
+export const Home = () => {
+    return (
+        <div>      
+            <Banner />
+            <About />
+            <Plans />
+        </div>
+    )
+}
+export default Home
