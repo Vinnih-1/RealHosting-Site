07fce688799d9515ce7ccd981654102d101b1448
Mikael <mikaelcandido71@gmail.com>
2023-01-05 16:08:34 -0300

    Chamando componentes que serão usados na página home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@
 import Banner from "./../Components/Layout/Banner/Banner";
 import About from "./../Components/Layout/About/About";
 import Plans from "./../Components/Layout/Plans/Plans";
+import Testimony from "../Components/Layout/Testimony/Testimony";
+import Question from "../Components/Layout/Question/Question";
 export const Home = () => {
     return (
         <div>      
             <Banner />
             <About />
             <Plans />
+            <Testimony />
+            <Question />
         </div>
     )
 }
