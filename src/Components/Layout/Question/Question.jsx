a8d711bd7b62833b531568f7dfd8fb74d9d3e7bf
Mikael <mikaelcandido71@gmail.com>
2023-01-05 16:13:43 -0300

    Iniciando o componente Question

---
+++ src/Components/Layout/Question/Question.jsx
@@ -0,0 +1,16 @@
+import './Question.css'
+export const Question = ()=>{
+    return(
+        <section>
+            <h1>Perguntas Frequentes</h1>
+            <div>
+           
+                <select>
+                    <option label='aaaaa'></option>
+                    <option>bbbbb</option>
+                </select>
+            </div>
+        </section>
+    )
+}
+export default Question
