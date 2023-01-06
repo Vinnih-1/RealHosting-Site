44f1a6cdbc233bfd18b6880e586f186a16f2753f
Mikael <mikaelcandido71@gmail.com>
2023-01-06 16:05:48 -0300

    Arrumando componente Question
    Criando o titulo e o container e chamando o componente Faq

--- src/Components/Layout/Question/Question.jsx
+++ src/Components/Layout/Question/Question.jsx
@@ -1,16 +1,14 @@
+import { Faq } from '../../Faq/Faq'
 import './Question.css'
-export const Question = ()=>{
-    return(
-        <section>
-            <h1>Perguntas Frequentes</h1>
-            <div>
-           
-                <select>
-                    <option label='aaaaa'></option>
-                    <option>bbbbb</option>
-                </select>
+export const Question = () => {
+    return (
+        <section className='section_question'>
+            <h1 className='title_question'>Perguntas Frequentes</h1>
+            <div className='container_faq'>
+                <Faq />
+                <Faq />
+                <Faq />
             </div>
         </section>
     )
 }
-export default Question
