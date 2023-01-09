9b03d28af398156717b4e51d47ac7635f4feb25b
Mikael <mikaelcandido71@gmail.com>
2023-01-09 16:32:25 -0300

    Importando o Warn e passando os parâmetros title e text

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,11 @@
-export const Services = ()=>{
-    return(
-        <h1>Serviços do zummys e AZULLLL</h1>
+import { Warn } from "../components/Layout/Warn/Warn"
+
+export const Services = () => {
+    return (
+        <div>
+            <Warn title="Escolha um dos nossos serviços
+e aproveite nossas qualidades " text="Promova hoje seu servidor com uma hospedagem com uma
+alta qualidade e proteção contra qualquer tipo de ataque."/>
+        </div>
     )
 }
