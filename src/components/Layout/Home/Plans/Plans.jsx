ddf933ac1b5137edd81f79f2bcf21bc98c886c5c
Vinnih-1 <vinifantal@hotmail.com>
2023-01-16 13:05:47 -0300

    adjust justify content and remove carousel

--- src/components/Layout/Home/Plans/Plans.jsx
+++ src/components/Layout/Home/Plans/Plans.jsx
@@ -1,58 +1,29 @@
-import { useEffect, useState, useRef } from 'react'
-import { motion } from 'framer-motion'
 import { Card } from '../../../Card/Card'
 import './Plans.css'
 export const Plans = () => {
-    const carousel = useRef();
-    const [width, setWidth] = useState();
-
-    useEffect(() => {
-        setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
-    }, [])
-
     return (
-        <motion.section className="section_plans" ref={carousel} whileTap={{ cursor: 'grabbing' }}>
-            <motion.div className='container_plans'>
+        <div className="section_plans">
+            <div className='container_plans'>
                 <h1 className='title_plans'>Confira os nossos principais plano totalmente ideal para o seu projeto</h1>
                 <p className='text_plans'>Promova hoje seu servidor com uma
                     hospedagem com umaalta qualidade e proteção
                     contra qualquer tipo de ataque.
                 </p>
-            </motion.div>
-            <motion.div className='card_plans'
-                drag="x"
-                dragConstraints={{ right: 0, left: -width }}
-                initial={{ x: 100 }}
-                animate={{ x: 0 }}>
+            </div>
+            <div className='card_plans'>
                 <Card
-                    image='https://cdn.discordapp.com/attachments/701489669673844887/1060199537483980860/image_4.png'
-                    title='Hospedagem MTA'
-                    price='9,99'
-                    text='Servidores MTA com um sistema 
-                inovador de compactador e download
-                e hospedagem totalmente Brasileira.'
-                    color="white"
-                />
-                <Card
-                    image='https://cdn.discordapp.com/attachments/701489669673844887/1060210162754932776/image_5.png'
+                    image='https://cdn.discordapp.com/attachments/835219525825462272/1063233848021897296/Minecraft_Grass-512x512_1.png'
                     title='Hospedagem de Minecraft'
-                    price='9,99'
-                    text='Servidores de Minecraft com ótima
-                facilidade de administrar e criar
-                rapidamente com ping Brasileiro.'
-                    color="white"
+                    price='2,50'
+                    text='Servidores de Minecraft com ótima facilidade de administrar e criar rapidamente com ping estável.'
                 />
                 <Card
-                    image='https://cdn.discordapp.com/attachments/701489669673844887/1060210543220228106/image_5_1.png'
-                    title='Hospedagem de FiveM  '
-                    price='9,99'
-                    text='Crie seu servidor RP com ótimas
-                qualidades e proteções para sua
-                 comunidade e divirta-se'
-                    color="white"
+                    image='https://cdn.discordapp.com/attachments/835219525825462272/1064438288800293004/discord.png'
+                    title='Hospedagem de Bot'
+                    price='1,25'
+                    text='Crie seu próprio bot de discord e hospede conosco com planos super baratos.'
                 />
-
-            </motion.div>
-        </motion.section>
+            </div>
+        </div>
     )
 }
